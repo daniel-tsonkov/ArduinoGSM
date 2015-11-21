3473bd3651d50080804467531fa70a80c962f0ff
danie <daniel_tzonkov@abv.bg>
2015-11-21 18:47:21 +0200

    Add ino

--- backButton.ino
+++
@@ -1,8 +0,0 @@
-void backButton(){
-  color150150150();
-  myGLCD.fillRoundRect(1, 740, 479, 785);
-  myGLCD.setFont(BigFont);
-  backColor150150150();
-  colorBlack();
-  myGLCD.print("B a c k", CENTER, 755);
-}
